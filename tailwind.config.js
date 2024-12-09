45da5bb45307b4cd4001bf29e8e3b6e155c635f1
LeGion <mehrdad.dadashi@gmail.com>
2024-12-09 18:45:59 +0300

    Complete chapter 3: Typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: [],
   theme: {
+    fontFamily: {
+      vazir: "vazir",
+    },
     extend: {
       padding: {
         20: "20px",
@@ -9,9 +12,25 @@ module.exports = {
       margin: {
         13: "13px",
       },
-      space:{
-        '6.5':'6.5px'
-      }
+      space: {
+        6.5: "6.5px",
+      },
+      fontSize: {
+        10: [
+          "10rem",
+          {
+            lineHeight: "5",
+            letterSpacing: ".3",
+            fontWeight: "bold",
+          },
+        ],
+      },
+      listStyleImage: {
+        checked: "url(../src/checked.png)",
+      },
+      colors: {
+        "regal-blue": "#243c5a",
+      },
     },
   },
   plugins: [],
